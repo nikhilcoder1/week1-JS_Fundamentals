d23d0a9b576187fb0b92eed2f8412f9ddf6b57a3
dev_nick <developernikhil28@gmail.com>
2025-12-16 14:16:44 +0530

    Initial Day4 mini project

---
+++ day_4_mini_project.js
@@ -0,0 +1,13 @@
+console.log("*********************User Profile Card Builder**********************")
+
+const user = {
+    name : "Nikhil",
+    age : 22,
+    skills : {
+        technical : ["JS" , "React" , "Tailwind CSS" , "JAVA"] ,
+        soft : ["Leadership" , "Cooperation" , "Collaboration"]
+    },
+    
+}
+
+console.log(user)
